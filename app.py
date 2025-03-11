7203bc3feda7cfd47c49e0dd0b422473542668c2
Pushya26 <146053954+Pushya26@users.noreply.github.com>
2025-03-11 21:24:51 +0530

    app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/htop')
 def htop():
-    username = os.getlogin()
-    ist_time = datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S')
-    top_output = subprocess.check_output(['top', '-b', '-n', '1']).decode('utf-8')
+    username = os.getenv("USER")
+    ist_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S IST')
+    top_output = subprocess.getoutput("top -b -n 1| head -10")
 
-    html_content = f"""
+    return f"""
     <h1>System Information</h1>
     <p><strong>Name:</strong> B C Pushya</p>
     <p><strong>Username:</strong> pushy</p>
@@ -20,10 +20,5 @@ def htop():
     <pre>{top_output}</pre>
     """
     
-    return render_template_string(html_content)
-@app.route('/health')
-def health():
-    return "Server is running!"
-
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
